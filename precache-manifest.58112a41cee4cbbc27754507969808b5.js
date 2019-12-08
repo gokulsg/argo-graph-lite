00ffeee673a34451c20656cc1766caa71c637cb9
RSLi <robertsli@163.com>
2019-12-08 02:40:50 -0500

    Updates

---
+++ precache-manifest.58112a41cee4cbbc27754507969808b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e733ab4feb89b73194168b35de1b63a7",
+    "url": "/argo-graph-lite/index.html"
+  },
+  {
+    "revision": "0a9a0c63f8142858dd30",
+    "url": "/argo-graph-lite/static/css/2.e81f0f91.chunk.css"
+  },
+  {
+    "revision": "29c4e03e79b2d7606430",
+    "url": "/argo-graph-lite/static/css/main.fccc4950.chunk.css"
+  },
+  {
+    "revision": "0a9a0c63f8142858dd30",
+    "url": "/argo-graph-lite/static/js/2.63f18b2b.chunk.js"
+  },
+  {
+    "revision": "29c4e03e79b2d7606430",
+    "url": "/argo-graph-lite/static/js/main.657db609.chunk.js"
+  },
+  {
+    "revision": "d266ead501e3cff44520",
+    "url": "/argo-graph-lite/static/js/runtime~main.7ffe45a9.js"
+  },
+  {
+    "revision": "9f52e4014adab067c8ef0f48ec1a4f43",
+    "url": "/argo-graph-lite/static/media/icons-16.9f52e401.eot"
+  },
+  {
+    "revision": "b49a90ee8c246157637b5ed142005d12",
+    "url": "/argo-graph-lite/static/media/icons-16.b49a90ee.woff"
+  },
+  {
+    "revision": "d75e6370295e046dd0b170ee42c3fa44",
+    "url": "/argo-graph-lite/static/media/icons-16.d75e6370.ttf"
+  },
+  {
+    "revision": "0f7149769d6b4ee77b646ee5b90c005a",
+    "url": "/argo-graph-lite/static/media/icons-20.0f714976.ttf"
+  },
+  {
+    "revision": "9baec0ab0bc3096fb9fe865d6ae29d1d",
+    "url": "/argo-graph-lite/static/media/icons-20.9baec0ab.woff"
+  },
+  {
+    "revision": "c6e7874eefb0dacfd50dad73a2de5a17",
+    "url": "/argo-graph-lite/static/media/icons-20.c6e7874e.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/argo-graph-lite/precache-manifest.b5ebf52c7695e0e2bb45c32aad07df67.js"
+  "/argo-graph-lite/precache-manifest.58112a41cee4cbbc27754507969808b5.js"
 );
 
 self.addEventListener('message', (event) => {
